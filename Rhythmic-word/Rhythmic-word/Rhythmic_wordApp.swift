2c9afc22d8d78a369ef9b445e498b9033aa06edf
isGeekCode <h1guitar@naver.com>
2025-04-11 10:47:20 +0900

    [ADD] 샘플 프로젝트 추가

---
+++ Rhythmic-word/Rhythmic-word/Rhythmic_wordApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rhythmic_wordApp.swift
+//  Rhythmic-word
+//
+//  Created by 방현석 on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Rhythmic_wordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Rhythmic-word/Rhythmic-wordTests/Rhythmic_wordTests.swift
@@ -0,0 +1,17 @@
+//
+//  Rhythmic_wordTests.swift
+//  Rhythmic-wordTests
+//
+//  Created by 방현석 on 4/11/25.
+//
+
+import Testing
+@testable import Rhythmic_word
+
+struct Rhythmic_wordTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
